e782d8657a2e824c808f98e7cc574c0bbe8e0162
YutaNagaoka <shirasu.okatin@gmail.com>
2019-06-14 00:01:17 +0900

    added zsh settings

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,14 @@
+alias zshrc='$EDITOR ~/.zshrc'
+
+# commands {{{
+alias la="ls -a"
+alias ll="ls -la"
+# }}}
+
+# git {{{
+alias ga="git add ."
+alias gc="git commit"
+alias gp="git push"
+alias gl="git log --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso"
+alias gs="git status"
+# }}}
