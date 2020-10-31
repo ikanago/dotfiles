ed296fa9acaa333440611ec3b8874f5dd8825dc9
ikanago <28985004+ikanago@users.noreply.github.com>
2020-10-31 17:34:32 +0900

    delete: Unused files

--- etc/init/gvm.sh
+++
@@ -1,2 +0,0 @@
-sudo apt install -y gcc bison
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer))
--- etc/init/neovim.sh
+++
@@ -1,4 +0,0 @@
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
-nvim --version
