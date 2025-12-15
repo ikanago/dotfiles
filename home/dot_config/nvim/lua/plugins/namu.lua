f90608ddb6222c72bcb084a820a37c92e409358c
ikanago <me@ikanago.dev>
2025-12-15 23:10:53 +0900

    Fix keymap config for namu

--- home/dot_config/nvim/lua/plugins/namu.lua
+++ home/dot_config/nvim/lua/plugins/namu.lua
@@ -1,8 +1,8 @@
 return {
     "bassamsdata/namu.nvim",
     keys = {
-        { "<leader>ss", "<CMD>Namu symbols<CR>",   desc = "Jump to LSP symbol" },
-        { "<leader>sw", "<CMD>Namu workspace<CR>", desc = "Jump to LSP symbol in the workspace" },
+        { "<leader>ss", "<CMD>Namu symbols<CR>",   mode = { "n" }, desc = "Jump to LSP symbol" },
+        { "<leader>sw", "<CMD>Namu workspace<CR>", mode = { "n" }, desc = "Jump to LSP symbol in the workspace" },
     },
     config = function()
         require("namu").setup({
