686fa1f687defbc8afd15281910d02e0a69b6444
ikanago <me@ikanago.dev>
2026-03-04 18:52:38 +0900

    Ring a sound when hooked

--- home/dot_claude/scripts/claude_hook.sh
+++ home/dot_claude/scripts/claude_hook.sh
@@ -14,6 +14,11 @@ _update_tmux_window_status() {
     tmux set-option -w -t "$window_id" @claude_status "$emoji" 2> /dev/null
 }
 
+_notify() {
+    notification_sound_path="/System/Library/Sounds/Purr.aiff"
+    [ -e $notification_sound_path ] && afplay $notification_sound_path
+}
+
 input=$(cat)
 
 hook_event=$(echo "$input" | jq -r '.hook_event_name // empty')
@@ -23,6 +28,7 @@ case "$hook_event" in
         notification_type=$(echo "$input" | jq -r '.notification_type // empty')
         if [ "$notification_type" = "permission_prompt" ]; then
             _update_tmux_window_status "🔴"
+            _notify
         fi
         ;;
     PostToolUse | UserPromptSubmit)
@@ -30,6 +36,7 @@ case "$hook_event" in
         ;;
     Stop)
         _update_tmux_window_status "🟢"
+        _notify
         ;;
     SessionEnd)
         _update_tmux_window_status ""
