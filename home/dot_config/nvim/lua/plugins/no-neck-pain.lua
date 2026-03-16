caf3049d53c084f78177f21cc963be7a5b4c7d96
ikanago <nagaoka.yuta@moneyforward.co.jp>
2026-03-16 18:56:45 +0900

    Disable no-neck-pain on startup

--- home/dot_config/nvim/lua/plugins/no-neck-pain.lua
+++ home/dot_config/nvim/lua/plugins/no-neck-pain.lua
@@ -4,10 +4,6 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("no-neck-pain").setup({
-            autocmds = {
-                enableOnVimEnter = true,
-                enableOnTabEnter = true,
-            },
             width = 120,
         })
     end
